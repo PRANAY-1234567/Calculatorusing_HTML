5461462a0729ab3b3863cd84af58b9821bbc00e7
Pranay Jadhao <pranayvjadhao197@gmail.com>
2026-07-31 22:45:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,6 @@ def calculator():
             result = num1 / num2 if num2 !=0 else 'Error'
             operation =f"{num1} / {num2} = {result}"
 
-
     #Push to firebase Realtime Database
         if result != 'Error':
             ref = db.reference('calculations')
